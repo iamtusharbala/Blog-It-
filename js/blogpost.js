5cee433ae80eb6ee89dc430bb5e610caa2fd7446
Tushar Balakrishnan <tusharbala284@gmail.com>
2020-06-04 03:46:39 +0530

    some changes added

---
+++ js/blogpost.js
@@ -0,0 +1,16 @@
+function postblog(){
+    var auth_name=document.getElementById('author-name').textContent;
+    var user_name = document.getElementById('username').textContent;
+    var blog = document.getElementById('blog-post').value;
+    document.getElementById('blog-posted').innerHTML = blog;
+    document.getElementById('user-title').innerHTML = auth_name;
+    document.getElementById('user-username').innerHTML = user_name;
+
+    var msg = document.getElementById('blog-msg')
+
+    if (blog === ""){
+        msg.className = "alert alert-danger"
+        msg.innerHTML = "Cannot leave post empty!!"
+    }
+
+}
